7d98fcf671c77f63e87a234730f646837475aa43
tleavitt25526 <tleavitt25526@gmail.com>
2024-10-14 14:36:25 -0400

    Schema tables added

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { Pool } = require("pg");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const pool = new Pool(
+  {
+    // TODO: Enter PostgreSQL username
+    user: 'postgres',
+    // TODO: Enter PostgreSQL password
+    password: 'miquella',
+    host: "localhost",
+    database: "employee_db",
+  },
+  console.log(`Connected to the employee database.`)
+);
+
+pool.connect();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+/*
+took 20 minutes to find the command line login for psql:
+psql -U postgres
+CREATE DATABASE employee_db
+*/
